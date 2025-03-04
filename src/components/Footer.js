1e6715fc1905cb8b13ee2620640f14d46006fc5a
Rafael Almeida <rafaelalmeida53@gmail.com>
2025-03-04 15:02:49 +0800

    Footer improved with cta button, facebook, sms and other pages links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,35 @@
 import styles from './Footer.module.css';
+import { Link } from "react-router-dom";
+import { FaFacebook, FaPhoneAlt } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2025 Profresh Carpet Cleaning - All rights reserved.</p>
+      <div className={styles.footerContent}>
+        {/* Navigation Links */}
+        <div className={styles.links}>
+          <Link to="/">Home</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+
+        {/* CTA Button */}
+        <div className={styles.cta}>
+          <Link to="/contact" className={styles.ctaButton}>Get a Free Quote</Link>
+        </div>
+
+        {/* Social & Contact */}
+        <div className={styles.social}>
+          <a href="https://www.facebook.com/yourpage" target="_blank" rel="noopener noreferrer">
+            <FaFacebook className={styles.icon} /> Facebook
+          </a>
+          <a href="sms:+61406963152">
+            <FaPhoneAlt className={styles.icon} /> Send SMS
+          </a>
+        </div>
+      </div>
+
+      <p>© {new Date().getFullYear()} Profresh Carpet Cleaning - All rights reserved.</p>
     </footer>
   );
 }
