b2016036a8d53e59ce2714749e6846c6d0e5b22b
Rafael Almeida <rafaelalmeida53@gmail.com>
2025-06-05 21:14:48 +0800

    face e sms no header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { FaBars, FaTimes, FaFacebook, FaSms } from 'react-icons/fa';
 import styles from './Header.module.css';
 
 function Header() {
@@ -9,21 +9,39 @@ function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
+        {/* Logo */}
         <div className={styles.logo}>
           <Link to="/" onClick={() => setMenuOpen(false)}>Profresh Carpet Cleaning</Link>
         </div>
 
-        {/* Hamburger Menu - Only Shows on Mobile */}
-        <div className={`${styles.menuIcon} ${menuOpen ? styles.open : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+        {/* Social icons below logo */}
+        <div className={styles.socialBar}>
+          <a
+            href="https://www.facebook.com/profile.php?id=61572791712252"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook className={styles.icon} /> Facebook
+          </a>
+          <a href="sms:+61431361230">
+            <FaSms className={styles.icon} /> SMS
+          </a>
+        </div>
+
+        {/* Hamburger menu (mobile) */}
+        <div
+          className={`${styles.menuIcon} ${menuOpen ? styles.open : ''}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
           {menuOpen ? <FaTimes className={styles.icon} /> : <FaBars className={styles.icon} />}
         </div>
 
-        {/* Desktop & Mobile Navigation */}
+        {/* Navigation */}
         <nav className={`${styles.nav} ${menuOpen ? styles.showMenu : ''}`}>
           <ul>
             <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
             <li><Link to="/services" onClick={() => setMenuOpen(false)}>Services</Link></li>
-            <li><Link to="/results" onClick={() => setMenuOpen(false)}>Results</Link></li>   
+            <li><Link to="/results" onClick={() => setMenuOpen(false)}>Results</Link></li>
             <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
           </ul>
         </nav>
